0bca50c13ef70d581732638833baeeb597a7743d
nl253 <nl253@kent.ac.uk>
2017-12-25 21:47:55 +0000

    Add BaseCipherTest for JUnit5

---
+++ java/cryptography/ciphers/BaseCipherTest.java
@@ -0,0 +1,26 @@
+package cryptography.ciphers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"ClassHasNoToStringMethod", "FeatureEnvy", "CallToSuspiciousStringMethod"})
+class BaseCipherTest {
+
+    @SuppressWarnings("PackageVisibleField")
+    BaseCipher cipher;
+
+    @Test
+    final void encode() {
+        final String randomMsg = BaseCipher.randomMsg();
+        Assertions
+                .assertEquals(cipher.decode(cipher.encode(randomMsg)), randomMsg, "Encoding / Decoding failed");
+    }
+
+    @Test
+    final void decode() {
+        final String encoded = cipher.encode();
+        Assertions
+                .assertEquals(cipher.encode(cipher.decode(encoded)), encoded, "Encoding / Decoding failed");
+    }
+
+}
--- java/cryptography/ciphers/CaesarCipherTest.java
+++ java/cryptography/ciphers/CaesarCipherTest.java
@@ -1,27 +1,11 @@
 package cryptography.ciphers;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-final class CaesarCipherTest {
-
-    private CaesarCipher cipher;
-
+final class CaesarCipherTest extends BaseCipherTest {
 
     @BeforeEach
     void setUp() {
         cipher = new CaesarCipher();
     }
-
-    @Test
-    void encode() {
-        CaesarCipher.decode(CaesarCipher.encode());
-    }
-
-    @Test
-    void decode() {
-        final String encoded = CaesarCipher.encode();
-        CaesarCipher.decode(encoded).equals(CaesarCipher.message);
-    }
-
 }
